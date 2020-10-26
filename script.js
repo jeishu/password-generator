806ee959ef0e0fcae29438d8fdcd0cc98c652f4b
jeishu <jeremy.zhu@utexas.edu>
2020-10-26 17:37:02 -0500

    added pseudo code for myself to know what I will be creating

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Pseudo Code
+// 1. create arrays for letters, numbers, and special characters.
+// 2. create a prompt for user input how many characters they want for their password.
+// 3. create a filter (confirm) for which characters they want to include or exclude.
+// 4. create a variable to contain the user input
+// 5. create a for loop(s) to generate the randomize password 
+// 6. then push the ranomdized generated password to the text box.
