91b879195cb93d51b9e5530b1972ac138fe46d87
jeishu <jeremy.zhu@utexas.edu>
2020-10-26 17:56:22 -0500

    added four arrays and an user prompt

--- script.js
+++ script.js
@@ -10,6 +10,19 @@ function writePassword() {
 
 }
 
+function generatePassword () {
+  // arrays for the characters
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var number = ["0","1","2","3","4","5","6","7","8","9"]
+  var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
+
+  // prompt for the user to input a number
+  var userInput = prompt("Please choose a number between 8 and 128"); 
+
+  
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
