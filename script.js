72dbcc6051779b99c5097f0b12355fefda9d1574
jeishu <jeremy.zhu@utexas.edu>
2020-10-26 18:18:55 -0500

    added if else statements to ensure the user inputs the apprioprate number, added confirms for password filter

--- script.js
+++ script.js
@@ -14,12 +14,35 @@ function generatePassword () {
   // arrays for the characters
   var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var number = ["0","1","2","3","4","5","6","7","8","9"]
+  var number = [0,1,2,3,4,5,6,7,8,9];
   var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
 
   // prompt for the user to input a number
-  var userInput = prompt("Please choose a number between 8 and 128"); 
+  var userInput = prompt("Please choose a number between 8 and 128."); 
 
+  // convert the string from the user input into an integer
+  var numInput = +userInput;
+  
+  // Making sure the user inouts a number that is between 8 and 128.
+  if ( userInput < 8 || userInput > 128 || !numInput) {
+    alert("Please choose a number that is between 8 and 128.");
+  }
+  else {
+    alert("Your password will be " + numInput + " characters long.");
+  }
+
+  // filters for your password
+  var filterLower = confirm("Do you want to have lowercase letters in your password?");
+  var filterUpper = confirm("Do you want to have uppercase letters in your password?");
+  var filterNumber = confirm("Do you want to have numbers in your password?");
+  var filterSymbols = confirm("Do you want to have symbols in your password?");
+
+  
+
+  // user has to choose at least one of the confirms above
+  if (!filterLower && !filterUpper && !filterNumber && !filterSymbols) {
+    alert("Please choose at least one of the conditions in order to generate your password.");
+  }
   
 }
 
