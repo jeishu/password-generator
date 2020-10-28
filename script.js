2341e70a85fdcb2c12afcac0824acc44da922964
jeishu <jeremy.zhu@utexas.edu>
2020-10-28 17:01:06 -0500

    clean up the code a bit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Variables for Array
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = [0,1,2,3,4,5,6,7,8,9];
+var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  
-
 }
 
 function generatePassword () {
@@ -14,33 +22,27 @@ function generatePassword () {
 
   var password = [];
 
-  // arrays for the characters
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var number = [0,1,2,3,4,5,6,7,8,9];
-  var symbols = ["!","@","#","$","%","^","&","*","(",")","{","}","[","]","<",">","."];
+  // prompt for the user to input a number || convert the string from the user input into an integer
+  var userInput = parseInt(prompt("Please choose a number between 8 and 128.")); 
 
-  // prompt for the user to input a number
-  var userInput = prompt("Please choose a number between 8 and 128."); 
-
-  // convert the string from the user input into an integer
-  var numInput = +userInput;
-
-  // Making sure the user inouts a number that is between 8 and 128.
-  if ( userInput < 8 || userInput > 128 || !numInput) {
+  // Making sure the user inputs a number that is between 8 and 128.
+  if (!userInput) {
+    alert("Please input a value that is a number.");
+    return;
+  }
+  else if ( userInput < 8 || userInput > 128) {
     alert("Please choose a number that is between 8 and 128.");
     return;
   }
   else {
-    alert("Your password will be " + numInput + " characters long.");
+    alert("Your password will be " + userInput + " characters long.");
+    
+    // filters for your password
+    var filterLower = confirm("Do you want to have lowercase letters in your password?");
+    var filterUpper = confirm("Do you want to have uppercase letters in your password?");
+    var filterNumber = confirm("Do you want to have numbers in your password?");
+    var filterSymbols = confirm("Do you want to have symbols in your password?");
   }
-
-  // filters for your password
-  var filterLower = confirm("Do you want to have lowercase letters in your password?");
-  var filterUpper = confirm("Do you want to have uppercase letters in your password?");
-  var filterNumber = confirm("Do you want to have numbers in your password?");
-  var filterSymbols = confirm("Do you want to have symbols in your password?");
-
 
   // user has to choose at least one of the confirms above
   if (!filterLower && !filterUpper && !filterNumber && !filterSymbols) {
@@ -49,28 +51,29 @@ function generatePassword () {
   }
   
   // create for loops for each of the confirms whether they are true or false
-  if (filterLower === true){
+  if (filterLower === true) {
     for (var i = 0; i < lowerCase.length; i++) {
       passwordHolder.push(lowerCase[i]);
     }
   }
-  if (filterUpper === true){
+  if (filterUpper === true) {
     for (var i = 0; i < upperCase.length; i++) {
       passwordHolder.push(upperCase[i]);
     }
   }
-  if (filterNumber === true){
+  if (filterNumber === true) {
     for (var i = 0; i < number.length; i++) {
       passwordHolder.push(number[i]);
     }
   }
-  if (filterSymbols === true){
+  if (filterSymbols === true) {
     for (var i = 0; i < symbols.length; i++) {
       passwordHolder.push(symbols[i]);
     }
   }
 
-  for (var i = 0; i < numInput; i++){
+  // this randomizes the password
+  for (var i = 0; i < userInput; i++){
     var random = Math.floor(Math.random() * passwordHolder.length);
 
     password = password + passwordHolder[random];
@@ -80,9 +83,6 @@ function generatePassword () {
 
   passwordText.value = password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // Pseudo Code
 // 1. create arrays for letters, numbers, and special characters.
